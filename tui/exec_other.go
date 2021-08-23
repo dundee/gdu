b1f492cff177b1b643580af328e9f93c6bbe734f
Daniel Milde <daniel@milde.cz>
2021-08-23 23:10:43 +0200

    exec and shell files merged

--- tui/exec_other.go
+++ tui/exec_other.go
@@ -3,10 +3,20 @@
 package tui
 
 import (
+	"os"
 	"syscall"
 )
 
+// Execute runs given bin path via the Exec syscall
 func Execute(argv0 string, argv []string, envv []string) error {
 	// append argv0 to argv, as execve will make first argument the "binary name".
 	return syscall.Exec(argv0, append([]string{argv0}, argv...), envv)
+}
+
+func getShellBin() string {
+	shellbin, ok := os.LookupEnv("SHELL")
+	if !ok {
+		shellbin = "/bin/bash"
+	}
+	return shellbin
 }
--- tui/exec_windows.go
+++ tui/exec_windows.go
@@ -5,6 +5,7 @@ import (
 	"os/exec"
 )
 
+// Execute runs given bin path via exec.Command call
 func Execute(argv0 string, argv []string, envv []string) error {
 	// Windows does not support exec syscall.
 	cmd := exec.Command(argv0, argv...)
@@ -17,4 +18,12 @@ func Execute(argv0 string, argv []string, envv []string) error {
 		os.Exit(0)
 	}
 	return err
+}
+
+func getShellBin() string {
+	shellbin, ok := os.LookupEnv("COMSPEC")
+	if !ok {
+		shellbin = "C:\\WINDOWS\\System32\\cmd.exe"
+	}
+	return shellbin
 }
