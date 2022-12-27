80c9061bd5b624984a6b097cddcb86581c56a370
Daniel Milde <daniel@milde.cz>
2022-12-27 10:26:59 +0100

    refactor: help text moved

--- tui/show.go
+++ tui/show.go
@@ -9,6 +9,34 @@ import (
 	"github.com/rivo/tview"
 )
 
+const helpText = `    [::b]up/down, k/j    [white:black:-]Move cursor up/down
+  [::b]pgup/pgdn, g/G    [white:black:-]Move cursor top/bottom
+ [::b]enter, right, l    [white:black:-]Go to directory/device
+         [::b]left, h    [white:black:-]Go to parent directory
+
+               [::b]r    [white:black:-]Rescan current directory
+               [::b]/    [white:black:-]Search items by name
+               [::b]a    [white:black:-]Toggle between showing disk usage and apparent size
+               [::b]B    [white:black:-]Toggle bar alignment to biggest file or directory
+               [::b]c    [white:black:-]Show/hide file count
+               [::b]m    [white:black:-]Show/hide latest mtime
+               [::b]b    [white:black:-]Spawn shell in current directory
+               [::b]q    [white:black:-]Quit gdu
+               [::b]Q    [white:black:-]Quit gdu and print current directory path
+
+Item under cursor:
+               [::b]d    [white:black:-]Delete file or directory
+               [::b]e    [white:black:-]Empty file or directory
+               [::b]v    [white:black:-]Show content of file
+               [::b]o    [white:black:-]Open file or directory in external program
+               [::b]i    [white:black:-]Show info about item
+
+Sort by (twice toggles asc/desc):
+               [::b]n    [white:black:-]Sort by name (asc/desc)
+               [::b]s    [white:black:-]Sort by size (asc/desc)
+               [::b]C    [white:black:-]Sort by file count (asc/desc)
+               [::b]M    [white:black:-]Sort by mtime (asc/desc)`
+
 func (ui *UI) showDir() {
 	var (
 		totalUsage int64
