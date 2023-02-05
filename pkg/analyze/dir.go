794028bfde9fa531c4c93cda329c389fb002ce53
Daniel Milde <daniel@milde.cz>
2023-02-05 21:06:03 +0100

    feat: follow symlinks in the analyzer

--- pkg/analyze/dir.go
+++ pkg/analyze/dir.go
@@ -22,6 +22,7 @@ type ParallelAnalyzer struct {
 	doneChan         common.SignalGroup
 	wait             *WaitGroup
 	ignoreDir        common.ShouldDirBeIgnored
+	followSymlinks   bool
 }
 
 // CreateAnalyzer returns Analyzer
@@ -39,12 +40,17 @@ func CreateAnalyzer() *ParallelAnalyzer {
 	}
 }
 
+// SetFollowSymlinks sets whether symlink to files should be followed
+func (a *ParallelAnalyzer) SetFollowSymlinks(v bool) {
+	a.followSymlinks = v
+}
+
 // GetProgressChan returns channel for getting progress
 func (a *ParallelAnalyzer) GetProgressChan() chan common.CurrentProgress {
 	return a.progressOutChan
 }
 
-// GetDoneChan returns channel for checking when analysis is done
+// GetDone returns channel for checking when analysis is done
 func (a *ParallelAnalyzer) GetDone() common.SignalGroup {
 	return a.doneChan
 }
@@ -132,6 +138,22 @@ func (a *ParallelAnalyzer) processDir(path string) *Dir {
 				log.Print(err.Error())
 				continue
 			}
+			if a.followSymlinks && info.Mode()&os.ModeSymlink != 0 {
+				target, err := filepath.EvalSymlinks(entryPath)
+				if err != nil {
+					log.Print(err.Error())
+					continue
+				}
+				tInfo, err := os.Lstat(target)
+				if err != nil {
+					log.Print(err.Error())
+					continue
+				}
+				if !tInfo.IsDir() {
+					info = tInfo
+				}
+			}
+
 			file = &File{
 				Name:   name,
 				Flag:   getFlag(info),
