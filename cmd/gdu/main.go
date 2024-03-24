d511c557bd2f745a3ca09d8677c09cf5386dbdbb
Daniel Milde <daniel@milde.cz>
2024-03-24 23:15:04 +0100

    fix: few improvements and fixes

--- cmd/gdu/main.go
+++ cmd/gdu/main.go
@@ -73,7 +73,7 @@ func init() {
 	flags.BoolVar(&af.UseSIPrefix, "si", false, "Show sizes with decimal SI prefixes (kB, MB, GB) instead of binary prefixes (KiB, MiB, GiB)")
 	flags.BoolVar(&af.NoPrefix, "no-prefix", false, "Show sizes as raw numbers without any prefixes (SI or binary) in non-interactive mode")
 	flags.BoolVar(&af.NoMouse, "no-mouse", false, "Do not use mouse")
-	flags.BoolVar(&af.NoDelete, "no-delete", false, "Do not allowe deletions")
+	flags.BoolVar(&af.NoDelete, "no-delete", false, "Do not allow deletions")
 	flags.BoolVar(&af.WriteConfig, "write-config", false, "Write current configuration to file (default is $HOME/.gdu.yaml)")
 
 	initConfig()
