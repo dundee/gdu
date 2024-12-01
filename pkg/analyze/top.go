a8b32c973b65bc26e7092c71d862efc05d31ffdb
Daniel Milde <daniel@milde.cz>
2024-12-01 11:12:16 +0100

    fix: top list

--- pkg/analyze/top.go
+++ pkg/analyze/top.go
@@ -20,18 +20,13 @@ func NewTopList(count int) *TopList {
 
 // Add adds file to the list
 func (tl *TopList) Add(file fs.Item) {
-	if len(tl.Items) < tl.Count {
+	if file.GetSize() > tl.MinSize || len(tl.Items) < tl.Count {
 		tl.Items = append(tl.Items, file)
-		if file.GetSize() > tl.MinSize {
-			tl.MinSize = file.GetSize()
-		}
-	} else if file.GetSize() > tl.MinSize {
-		tl.Items = append(tl.Items, file)
-		tl.MinSize = file.GetSize()
+		sort.Sort(fs.ByApparentSize(tl.Items))
 		if len(tl.Items) > tl.Count {
-			sort.Sort(fs.ByApparentSize(tl.Items))
 			tl.Items = tl.Items[1:]
 		}
+		tl.MinSize = tl.Items[0].GetSize()
 	}
 }
 
