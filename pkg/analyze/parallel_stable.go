2840ec6cb5c4372983240d2636828502ed469189
ShivamB25 <104637569+ShivamB25@users.noreply.github.com>
2025-11-22 19:14:05 +0530

    feat: introduce parallel analyzer with stable ordering (#445)
    * fix: preserve original file/dir order in ParallelAnalyzer and add ordering/determinism tests
    
    Use indexed items and a buffered channel to collect files and subdirs with their original indices,
    then append them in-order to preserve the original file/directory interleaving when running the
    parallel analyzer. This makes output deterministic and consistent with the sequential analyzer.
    
    Also add tests and helper:
    - TestParallelAnalyzerDeterminism
    - TestParallelVsSequentialConsistency
    - TestFileDirectoryInterleaving
    - getFileNames helper
    
    * feat: separate analyzer for stable sorting
    
    ---------
    
    Co-authored-by: Daniel Milde <daniel@milde.cz>

---
+++ pkg/analyze/parallel_stable.go
@@ -0,0 +1,224 @@
+package analyze
+
+import (
+	"os"
+	"path/filepath"
+	"runtime/debug"
+
+	"github.com/dundee/gdu/v5/internal/common"
+	"github.com/dundee/gdu/v5/pkg/fs"
+	log "github.com/sirupsen/logrus"
+)
+
+// ParallelStableOrderAnalyzer implements Analyzer
+type ParallelStableOrderAnalyzer struct {
+	progress         *common.CurrentProgress
+	progressChan     chan common.CurrentProgress
+	progressOutChan  chan common.CurrentProgress
+	progressDoneChan chan struct{}
+	doneChan         common.SignalGroup
+	wait             *WaitGroup
+	ignoreDir        common.ShouldDirBeIgnored
+	followSymlinks   bool
+	gitAnnexedSize   bool
+}
+
+// CreateStableOrderAnalyzer returns parallel Analyzer which keeps stable order of files
+func CreateStableOrderAnalyzer() *ParallelStableOrderAnalyzer {
+	return &ParallelStableOrderAnalyzer{
+		progress: &common.CurrentProgress{
+			ItemCount: 0,
+			TotalSize: int64(0),
+		},
+		progressChan:     make(chan common.CurrentProgress, 1),
+		progressOutChan:  make(chan common.CurrentProgress, 1),
+		progressDoneChan: make(chan struct{}),
+		doneChan:         make(common.SignalGroup),
+		wait:             (&WaitGroup{}).Init(),
+	}
+}
+
+// SetFollowSymlinks sets whether symlink to files should be followed
+func (a *ParallelStableOrderAnalyzer) SetFollowSymlinks(v bool) {
+	a.followSymlinks = v
+}
+
+// SetShowAnnexedSize sets whether to use annexed size of git-annex files
+func (a *ParallelStableOrderAnalyzer) SetShowAnnexedSize(v bool) {
+	a.gitAnnexedSize = v
+}
+
+// GetProgressChan returns channel for getting progress
+func (a *ParallelStableOrderAnalyzer) GetProgressChan() chan common.CurrentProgress {
+	return a.progressOutChan
+}
+
+// GetDone returns channel for checking when analysis is done
+func (a *ParallelStableOrderAnalyzer) GetDone() common.SignalGroup {
+	return a.doneChan
+}
+
+// ResetProgress returns progress
+func (a *ParallelStableOrderAnalyzer) ResetProgress() {
+	a.progress = &common.CurrentProgress{}
+	a.progressChan = make(chan common.CurrentProgress, 1)
+	a.progressOutChan = make(chan common.CurrentProgress, 1)
+	a.progressDoneChan = make(chan struct{})
+	a.doneChan = make(common.SignalGroup)
+	a.wait = (&WaitGroup{}).Init()
+}
+
+// AnalyzeDir analyzes given path
+func (a *ParallelStableOrderAnalyzer) AnalyzeDir(
+	path string, ignore common.ShouldDirBeIgnored, constGC bool,
+) fs.Item {
+	if !constGC {
+		defer debug.SetGCPercent(debug.SetGCPercent(-1))
+		go manageMemoryUsage(a.doneChan)
+	}
+
+	a.ignoreDir = ignore
+
+	go a.updateProgress()
+	dir := a.processDir(path)
+
+	dir.BasePath = filepath.Dir(path)
+	a.wait.Wait()
+
+	a.progressDoneChan <- struct{}{}
+	a.doneChan.Broadcast()
+
+	return dir
+}
+
+func (a *ParallelStableOrderAnalyzer) processDir(path string) *Dir {
+	type indexedItem struct {
+		index int
+		item  fs.Item
+	}
+
+	var (
+		file      *File
+		err       error
+		totalSize int64
+		info      os.FileInfo
+		itemCount int
+		dirCount  int
+	)
+
+	a.wait.Add(1)
+
+	files, err := os.ReadDir(path)
+	if err != nil {
+		log.Print(err.Error())
+	}
+
+	dir := &Dir{
+		File: &File{
+			Name: filepath.Base(path),
+			Flag: getDirFlag(err, len(files)),
+		},
+		ItemCount: 1,
+		Files:     make(fs.Files, 0, len(files)),
+	}
+	setDirPlatformSpecificAttrs(dir, path)
+
+	// Buffer channel to prevent deadlock when sending files synchronously
+	itemChan := make(chan indexedItem, len(files))
+
+	for _, f := range files {
+		name := f.Name()
+		entryPath := filepath.Join(path, name)
+		if f.IsDir() {
+			if a.ignoreDir(name, entryPath) {
+				continue
+			}
+			currentIndex := itemCount
+			itemCount++
+			dirCount++
+
+			go func(entryPath string, idx int) {
+				concurrencyLimit <- struct{}{}
+				subdir := a.processDir(entryPath)
+				subdir.Parent = dir
+
+				itemChan <- indexedItem{idx, subdir}
+				<-concurrencyLimit
+			}(entryPath, currentIndex)
+		} else {
+			info, err = f.Info()
+			if err != nil {
+				log.Print(err.Error())
+				dir.Flag = '!'
+				continue
+			}
+			if a.followSymlinks && info.Mode()&os.ModeSymlink != 0 {
+				infoF, err := followSymlink(entryPath, a.gitAnnexedSize)
+				if err != nil {
+					log.Print(err.Error())
+					dir.Flag = '!'
+					continue
+				}
+				if infoF != nil {
+					info = infoF
+				}
+			}
+
+			file = &File{
+				Name:   name,
+				Flag:   getFlag(info),
+				Size:   info.Size(),
+				Parent: dir,
+			}
+			setPlatformSpecificAttrs(file, info)
+
+			totalSize += info.Size()
+
+			// Send file to channel with its index
+			itemChan <- indexedItem{itemCount, file}
+			itemCount++
+		}
+	}
+
+	go func() {
+		items := make([]indexedItem, itemCount)
+
+		// Collect all items (both files and subdirs)
+		for i := 0; i < itemCount; i++ {
+			indexed := <-itemChan
+			items[indexed.index] = indexed
+		}
+
+		// Add all items in their original order
+		for i := 0; i < itemCount; i++ {
+			dir.AddFile(items[i].item)
+		}
+
+		a.wait.Done()
+	}()
+
+	a.progressChan <- common.CurrentProgress{
+		CurrentItemName: path,
+		ItemCount:       len(files),
+		TotalSize:       totalSize,
+	}
+	return dir
+}
+
+func (a *ParallelStableOrderAnalyzer) updateProgress() {
+	for {
+		select {
+		case <-a.progressDoneChan:
+			return
+		case progress := <-a.progressChan:
+			a.progress.CurrentItemName = progress.CurrentItemName
+			a.progress.ItemCount += progress.ItemCount
+			a.progress.TotalSize += progress.TotalSize
+		}
+
+		select {
+		case a.progressOutChan <- *a.progress:
+		default:
+		}
+	}
+}
