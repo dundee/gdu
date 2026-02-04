0aad7bef5cacc41c997c206139bce33585ab960e
sguptasf <173396169+sguptasf@users.noreply.github.com>
2026-02-05 01:41:43 +0530

    feat: add depth flag (#485)
    * add depth flag
    
    * Simplify
    
    * Add tests
    
    * Formatted output and print files
    
    * Fix description
    
    * Fix linux test and formatting
    
    * Add reverse-sort test
    
    * fix: nolint
    
    ---------
    
    Co-authored-by: Daniel Milde <daniel@milde.cz>

--- stdout/stdout.go
+++ stdout/stdout.go
@@ -25,6 +25,7 @@ type UI struct {
 	orange      *color.Color
 	blue        *color.Color
 	top         int
+	depth       int
 	summarize   bool
 	noPrefix    bool
 	fixedBase   float64
@@ -52,6 +53,7 @@ func CreateStdoutUI(
 	fixedUnit string,
 	top int,
 	reverseSort bool,
+	depth int,
 ) *UI {
 	ui := &UI{
 		UI: &common.UI{
@@ -68,6 +70,7 @@ func CreateStdoutUI(
 		noPrefix:    noPrefix,
 		top:         top,
 		reverseSort: reverseSort,
+		depth:       depth,
 	}
 	if fixedUnit != "" {
 		ui.SetFixedUnit(fixedUnit)
@@ -205,6 +208,8 @@ func (ui *UI) AnalyzePath(path string, _ fs.Item) error {
 	switch {
 	case ui.top > 0:
 		ui.printTopFiles(dir)
+	case ui.depth > 0:
+		ui.printDirWithDepth(dir, 0)
 	case ui.summarize:
 		ui.printTotalItem(dir)
 	default:
@@ -323,10 +328,45 @@ func (ui *UI) printItemPath(file fs.Item) {
 		size = file.GetUsage()
 	}
 
-	fmt.Fprintf(ui.output,
-		lineFormat,
-		ui.formatSize(size),
-		file.GetPath())
+	if file.IsDir() {
+		fmt.Fprintf(ui.output,
+			lineFormat,
+			ui.formatSize(size),
+			ui.blue.Sprint(file.GetPath()))
+	} else {
+		fmt.Fprintf(ui.output,
+			lineFormat,
+			ui.formatSize(size),
+			file.GetPath())
+	}
+}
+
+func (ui *UI) printDirWithDepth(dir fs.Item, currentDepth int) {
+	// Print current directory
+	ui.printItemPath(dir)
+
+	// If we haven't reached the max depth, print contents
+	if currentDepth < ui.depth && dir.IsDir() {
+		files := dir.GetFiles()
+
+		// Sort files
+		if ui.reverseSort {
+			sort.Sort(files)
+		} else {
+			sort.Sort(sort.Reverse(files))
+		}
+
+		// Print all files at this depth level
+		for _, file := range files {
+			if file.IsDir() {
+				// Recurse into subdirectories
+				ui.printDirWithDepth(file, currentDepth+1)
+			} else {
+				// Print regular files
+				ui.printItemPath(file)
+			}
+		}
+	}
 }
 
 // ReadAnalysis reads analysis report from JSON file
