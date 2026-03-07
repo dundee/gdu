3d550fefcf38efd38ef996fb43e1dcbaad6468f5
Daniel Milde <daniel@milde.cz>
2026-03-07 21:30:44 +0100

    fix: support for sqlite db on all platforms (#507)
    * fix: support for sqlite db on all platforms
    
    * fix: lint

--- pkg/analyze/sqlite_modernc.go
+++ pkg/analyze/sqlite_modernc.go
@@ -1,8 +1,13 @@
-//go:build (linux || darwin) && (amd64 || arm64 || loong64 || ppc64le || s390x || riscv64 || 386 || arm)
+//go:build (linux && !mips64 && !mipsle && !mips && !mips64le && !ppc64) || darwin || windows || (freebsd && !arm && !386) || (openbsd && !386) || (netbsd && !arm && !386 && !amd64)
 
 package analyze
 
 import (
-	// nolint:revive // use driver
+	// nolint:revive // Why: importing SQLite driver for side effects
 	_ "modernc.org/sqlite"
 )
+
+// checkAvailable checks if the modernc SQLite driver is available
+func checkAvailable() error {
+	return nil
+}
